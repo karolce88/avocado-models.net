80df34aeb3184e46ffd227f41c623d5f0b1d4ff8
Karol <karolcwiklik@o2.pl>
2023-03-07 10:52:12 +0100

    btn branch

--- js/script.js
+++ js/script.js
@@ -4,28 +4,38 @@ const itemFirst = document.querySelector('.item1')
 const itemThree = document.querySelector('.item3')
 const btnAnimDelay = document.querySelector('.burger-button')
 
+function btnClick() {
+	if (
+		itemTwo.classList.contains('item2-display-none') &&
+		itemFirst.classList.contains('item1-rotate') &&
+		itemThree.classList.contains('item3-rotate')
+	) {
+		itemTwo.classList.remove('item2-display-none')
+		itemFirst.classList.remove('item1-rotate')
+		itemThree.classList.remove('item3-rotate')
 
-function btnClick () {
-    let delayTime = 0;
+		itemTwo.classList.add('item2-display-none-rev')
+		itemFirst.classList.add('item1-rotate-rev')
+		itemThree.classList.add('item3-rotate-rev')
+	}
+	else if (
+		itemTwo.classList.contains('item2-display-none-rev') &&
+		itemFirst.classList.contains('item1-rotate-rev') &&
+		itemThree.classList.contains('item3-rotate-rev')
+	) {
+		itemTwo.classList.remove('item2-display-none-rev')
+		itemFirst.classList.remove('item1-rotate-rev')
+		itemThree.classList.remove('item3-rotate-rev')
 
-    itemTwo.classList.toggle('item2-display-none');
-    
-    itemFirst.classList.toggle('item1-rotate');
-    
-    itemThree.classList.toggle('item3-rotate');
-    
-    burgerBtn.classList.toggle('container-btn-border');
-    
-
-
-    btnAnimDelay.forEach(item => {
-        burgerBtn.style.animationDelay = '.' + delayTime + 's';
-        itemThree.style.animationDelay = '.' + delayTime + 's';
-        itemFirst.style.animationDelay = '.' + delayTime + 's';
-        itemTwo.style.animationDelay = '.' + delayTime + 's';
-
-        delayTime++;
-    })
+		itemTwo.classList.add('item2-display-none')
+		itemFirst.classList.add('item1-rotate')
+		itemThree.classList.add('item3-rotate')
+	} else {
+		itemTwo.classList.add('item2-display-none')
+		itemFirst.classList.add('item1-rotate')
+		itemThree.classList.add('item3-rotate')
+		burgerBtn.classList.toggle('container-btn-border')
+	}
 }
 
 burgerBtn.addEventListener('click', btnClick)
